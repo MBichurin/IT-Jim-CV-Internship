8168704d13e27cfe2f72b5a95b1545379dca52c7
MBichurin <bichurin.cmds@gmail.com>
2020-07-10 15:25:10 +0300

    Canny instead of AdaptGaussThreshold for EdgeDetection

--- Task1/Objects Enhancement.py
+++ Task1/Objects Enhancement.py
@@ -1,6 +1,19 @@
 import cv2
 import numpy as np
 
+def myEdgeSearch(frame_gray):
+    # Adaptive Threshold Gauss of Gray
+    frame_adaptGauss = cv2.adaptiveThreshold(frame_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # Inverse
+    frame_adaptGauss = 255 - frame_adaptGauss
+
+    # Opening
+    frame_open = cv2.erode(frame_adaptGauss, np.ones((3, 3), np.uint8), 1)
+    frame_open = cv2.dilate(frame_open, np.ones((3, 3), np.uint8), 1)
+
+    return frame_open
+
 if __name__ == '__main__':
     vid_name = 'input_video.avi'
     vid = cv2.VideoCapture(vid_name)
@@ -12,22 +25,26 @@ if __name__ == '__main__':
         # Resize
         frame_bgr = cv2.resize(frame_bgr, (int(frame_bgr.shape[1] * 0.8), int(frame_bgr.shape[0] * 0.8)))
 
-        # BGR 2 GRAY
+        # Converts
         frame_gray = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
-
-        # BGR 2 HSV
         frame_hsv = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2HSV)
-
-        # BGR 2 Lab
         frame_lab = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2Lab)
 
-        # Adaptive Threshold Gauss of Gray
-        frame_adaptGauss = cv2.adaptiveThreshold(frame_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+
+        # CannyV
+        frame_cannyV = cv2.Canny(frame_hsv[:, :, 2], 35, 45)
+
+
+
 
         # image = 2 versions of a frame
         image = np.zeros((frame_gray.shape[0] * 2, frame_gray.shape[1]), dtype=np.uint8)
-        image[:frame_gray.shape[0], :] = frame_gray[:, :]
-        image[frame_gray.shape[0]:, :] = frame_adaptGauss
+
+        image[:frame_gray.shape[0], :] = frame_gray
+
+        image[frame_gray.shape[0]:, :] = frame_cannyV
+
 
         cv2.imshow('Player', image)
         cv2.waitKey(30)
