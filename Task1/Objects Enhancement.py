8b82ed0edee59feaa96bc47c8a224ff22ed169df
MBichurin <bichurin.cmds@gmail.com>
2020-07-12 13:58:07 +0300

    Separate black objs detection

--- Task1/Objects Enhancement.py
+++ Task1/Objects Enhancement.py
@@ -1,19 +1,6 @@
 import cv2
 import numpy as np
 
-def myEdgeSearch(frame_gray):
-    # Adaptive Threshold Gauss of Gray
-    frame_adaptGauss = cv2.adaptiveThreshold(frame_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-
-    # Inverse
-    frame_adaptGauss = 255 - frame_adaptGauss
-
-    # Opening
-    frame_open = cv2.erode(frame_adaptGauss, np.ones((3, 3), np.uint8), 1)
-    frame_open = cv2.dilate(frame_open, np.ones((3, 3), np.uint8), 1)
-
-    return frame_open
-
 def cannyEdgeSearch(frame_hsv):
     # CannyV
     frame_cannyV = cv2.Canny(frame_hsv[:, :, 2], 35, 45)
@@ -31,10 +18,8 @@ def cannyEdgeSearch(frame_hsv):
     return frame_cannyV
 
 def getMask(frm_edges):
-    # Contours
+    # 2-lvl contours to ignore inners
     contours, hierarchy = cv2.findContours(frm_edges, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-
-    i_cnt = 0
 
     frame_cnts = np.zeros(frm_edges.shape, dtype=np.uint8)
 
@@ -42,17 +27,18 @@ def getMask(frm_edges):
     topBorder = np.zeros(frame_cnts.shape, dtype=np.uint8)
     topBorder[0, :] = 255
 
-    while i_cnt >= 0:  # the contour exists
+    i_cnt = 0
+    while i_cnt >= 0 and len(contours) > 0:  # the contour exists
         the_cnt = contours[i_cnt]
         # Perimeter
         P = cv2.arcLength(the_cnt, False)
 
-        if 100 < P < 1200:
+        if 100 < P < 1200: # Get rid of small and large objs
             # Put the contour on an image to check if it's on borders
             cnt_img = np.zeros(frame_cnts.shape, dtype=np.uint8)
             cnt_img = cv2.drawContours(cnt_img, [the_cnt], -1, (255, 255, 255), 1)
 
-            if ~cv2.bitwise_and(cnt_img, topBorder).any():
+            if ~cv2.bitwise_and(cnt_img, topBorder).any(): # Cnt doesn't touch borders
                 # Rotated Rectangle
                 (x, y), (width, height), angle = cv2.minAreaRect(the_cnt)
                 S_rect = width * height
@@ -60,7 +46,7 @@ def getMask(frm_edges):
                 if width > height:
                     width, height = height, width
 
-                if height / width < 1.5:
+                if width > 0 and height / width < 1.5: # Not prolate obj
                     frame_cnts = cv2.fillPoly(frame_cnts, [the_cnt], (255, 255, 255))
 
         # Next contour
@@ -68,30 +54,71 @@ def getMask(frm_edges):
     return frame_cnts
 
 def approxFigures(mask):
-    # Contours
+    # 1-lvl contours
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-
-    i_cnt = 0
 
     mask = np.zeros(mask.shape, dtype=np.uint8)
 
-    while i_cnt >= 0 and len(contours) > 0:  # the contour exists
+    i_cnt = 0
+    while i_cnt >= 0 and len(contours) > 0:  # cnt exists
         the_cnt = contours[i_cnt]
 
+        # Convex Hull
         hull = cv2.convexHull(the_cnt)
-
         mask = cv2.fillPoly(mask, [hull], (255, 255, 255))
 
         # Next contour
         i_cnt = hierarchy[0][i_cnt][0]
     return mask
 
+def findBlackObjs(frame_gray):
+    # Black clr only
+    black_mask = cv2.inRange(frame_gray, 0, 30)
+
+    # Open
+    black_mask = cv2.erode(black_mask, np.ones((7, 7), np.uint8))
+    black_mask = cv2.dilate(black_mask, np.ones((7, 7), np.uint8))
+
+    # 2-lvl contours to ignore inners
+    contours, hierarchy = cv2.findContours(black_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+
+    black_mask = np.zeros(black_mask.shape, dtype=np.uint8)
+
+    # Top border of frames
+    topBorder = np.zeros(black_mask.shape, dtype=np.uint8)
+    topBorder[0, :] = 255
+
+    i_cnt = 0
+    while i_cnt >= 0 and len(contours) > 0:  # the contour exists
+        the_cnt = contours[i_cnt]
+
+        # Area
+        S = cv2.contourArea(the_cnt)
+
+        if S > 3800:  # Get rid of small objs
+            # Put the contour on an image to check if it's on borders
+            cnt_img = np.zeros(black_mask.shape, dtype=np.uint8)
+            cnt_img = cv2.drawContours(cnt_img, [the_cnt], -1, (255, 255, 255), 1)
+
+            if ~cv2.bitwise_and(cnt_img, topBorder).any():  # Cnt doesn't touch borders
+                # Convex Hull
+                hull = cv2.convexHull(the_cnt)
+
+                black_mask = cv2.fillPoly(black_mask, [hull], (255, 255, 255))
+
+        # Next contour
+        i_cnt = hierarchy[0][i_cnt][0]
+
+    # Dilate
+    for i in range(2):
+        black_mask = cv2.dilate(black_mask, np.ones((3, 3), np.uint8))
+
+    return black_mask
+
 if __name__ == '__main__':
     vid_name = 'input_video.avi'
     vid = cv2.VideoCapture(vid_name)
     ret, blackframe = vid.read()
-
-    iter = 0
 
     while ret:
         # Get rid of black frame
@@ -120,10 +147,13 @@ if __name__ == '__main__':
         # Approx figures
         mask = approxFigures(mask)
 
+        # Black color
+        black_mask = findBlackObjs(frame_gray)
+
         # Upd the main image
         image = np.zeros((frame_gray.shape[0] * 2, frame_gray.shape[1]), dtype=np.uint8)
-        image[:frame_gray.shape[0], :] = mask
-        image[frame_gray.shape[0]:, :] = cv2.bitwise_or(frame_gray, mask)
+        image[:frame_gray.shape[0], :] = frame_gray
+        image[frame_gray.shape[0]:, :] = cv2.bitwise_or(cv2.bitwise_or(frame_gray, mask), black_mask)
 
         # Build a window
         cv2.imshow('Player', image)
@@ -132,6 +162,4 @@ if __name__ == '__main__':
 
         # Get the next frame
         ret, blackframe = vid.read()
-
-        iter += 1
     vid.release()
