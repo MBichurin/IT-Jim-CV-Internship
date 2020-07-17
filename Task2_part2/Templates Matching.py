fd7cc90a905ab4a8fefeb45822467a0a7bac9860
MBichurin <bichurin.cmds@gmail.com>
2020-07-17 17:14:06 +0300

    First tries of using matchTemplate()

--- Task2_part2/Templates Matching.py
+++ Task2_part2/Templates Matching.py
@@ -8,7 +8,25 @@ def matcher():
     img_bgr = cv2.imread(img_name)
     img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 
-    fig = plt.figure(figsize=(6, 6))
+    for i in range(16):
+        if i < 9:
+            tmp_name = 'symbols/00' + str(i + 1) + '.png'
+        else:
+            tmp_name = 'symbols/0' + str(i + 1) + '.png'
+        tmp = cv2.imread(tmp_name, cv2.IMREAD_GRAYSCALE)
+
+        res = cv2.matchTemplate(img_gray, tmp, cv2.TM_CCOEFF_NORMED)
+
+        background = np.copy(img_bgr)
+
+        loc = np.where(res >= 0.7)
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(background, pt, (pt[0] + tmp.shape[1], pt[1] + tmp.shape[0]), [0, 255, 0], 2)
+
+        plt.imshow(background)
+        plt.show()
+
+    '''fig = plt.figure(figsize=(6, 6))
 
     for i in range(16):
         if i < 9:
@@ -30,7 +48,9 @@ def matcher():
         axis = fig.add_subplot(4, 4, i + 1)
         axis.set_xticks([]), axis.set_yticks([])
         plt.imshow(tmp_bgr)
-    plt.show()
+    plt.show()'''
+
+
 
 
 if __name__ == '__main__':
