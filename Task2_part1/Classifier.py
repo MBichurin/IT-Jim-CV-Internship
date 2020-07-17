29c700e820ba64e36d0a35e209329adfd14d4d64
MBichurin <bichurin.cmds@gmail.com>
2020-07-17 13:47:22 +0300

    Text descriptions. Video output

--- Task2_part1/Classifier.py
+++ Task2_part1/Classifier.py
@@ -129,8 +129,10 @@ def findBlackObjs(frame_gray):
     return black_mask
 
 def classifyContour(cnt):
+    # Perimeter and Area
     P = cv2.arcLength(cnt, True)
     S = cv2.contourArea(cnt)
+    # Approximation and its area
     approx = cv2.approxPolyDP(cnt, 0.07 * P, True)
     S_approx = cv2.contourArea(approx)
 
@@ -138,15 +140,12 @@ def classifyContour(cnt):
     cnt_img = np.zeros(black_mask.shape, dtype=np.uint8)
     cv2.drawContours(cnt_img, [cnt], -1, (255, 255, 255), 1)
 
-    # # Output contour
-    # cv2.imshow('Another window', cnt_img)
-    # cv2.waitKey(250)
-
+    # Rotated rectangle
     (x, y), (w, h), angle = cv2.minAreaRect(cnt)
-
+    # Min enclosing circle
     (x, y), radius = cv2.minEnclosingCircle(cnt)
 
-    # Top border of frames
+    # Borders of frames
     borders = np.zeros(black_mask.shape, dtype=np.uint8)
     borders[0, :] = 255
     borders[:, 0] = 255
@@ -154,55 +153,65 @@ def classifyContour(cnt):
     borders[:, borders.shape[1] - 1] = 255
 
     if S < 500 or cv2.bitwise_and(cnt_img, borders).any():
-        # ???
+        # ??? <== the object is too small to be classified or it touches frame borders
         return -1
     elif w * h / S >= 1.4:
-        # triangle
+        # triangle <== the rotated rectangle's area is much bigger than the object's area
         return 0
     elif np.pi * radius * radius / S <= 1.365:
-        # circle
+        # circle <== the enclosing circle's area is almost the same as the object's area
         return 1
     elif w * h / S <= 1.09:
-        # rectangle
+        # rectangle <== the rotated rectangle's area is almost the same as the object's area
         return 2
     elif w * h / S_approx >= 1.8 and approx.shape[0] == 3:
-        # triangle
+        # triangle <== the approximation is a triangle and it's much smaller than the rotated rectangle's area
         return 0
     else:
+        # more rough approximation
         approx = cv2.approxPolyDP(cnt, 0.2 * P, True)
         if approx.shape[0] == 3:
-            # triangle
+            # triangle <== the approximation is a triangle
             return 0
         else:
+            # less rough approximation
             approx = cv2.approxPolyDP(cnt, 0.03 * P, True)
             (x, y), (w, h), angle = cv2.minAreaRect(approx)
             if w * h / cv2.contourArea(approx) <= 1.2 and approx.shape[0] == 4:
-                # rectangle
+                # rectangle <== the approximation is a quadrilateral and the rotated rectangle's area is almost the same as the object's area
                 return 2
             else:
-                # ??? but it'd better be improved
+                # ??? I can't divide the rest of objs because of the chosen detection strategy:(
                 return -1
 
 def classifyObjs(mask):
+    # Only outer contours
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
     classes = np.zeros((mask.shape[0], mask.shape[1], 3), dtype=np.uint8)
 
+    # Triangle, circle and rectangle colors
     colors = ((0, 255, 0), (153, 0, 153), (255, 102, 102))
+
+    # List of lists of centers' coordinates: [[<triangles' centers>], [<circles' centers>], [<rects' centers>]]
+    Centers = [[], [], []]
 
     i_cnt = 0
     while i_cnt >= 0 and len(contours) > 0:  # the contour exists
         the_cnt = contours[i_cnt]
 
+        # Classify the contour (0 = triangle, 1 = circle, 2 = rectangle)
         cnt_class = classifyContour(the_cnt)
 
         if cnt_class != -1:
             cv2.drawContours(classes, [the_cnt], -1, colors[cnt_class], 3)
+            M = cv2.moments(the_cnt)
+            Centers[cnt_class].append([int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])])
 
         # Next contour
         i_cnt = hierarchy[0][i_cnt][0]
 
-    return classes
+    return classes, Centers
 
 if __name__ == '__main__':
     vid_name = 'input_video.avi'
@@ -213,9 +222,7 @@ if __name__ == '__main__':
 
     # Writer
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    writer = cv2.VideoWriter('output_video.avi', fourcc, 20, (int(blackframe.shape[1] * 0.8), int(blackframe.shape[0] * 0.8)), 0)
-
-    #writer2 = cv2.VideoWriter('4windows.avi', fourcc, 20, (int(blackframe.shape[1] * 0.8) * 2, int(360 * 0.8) * 2))
+    writer = cv2.VideoWriter('output_video.avi', fourcc, 20, (int(blackframe.shape[1] * 0.8), int(blackframe.shape[0] * 0.8)))
 
     while ret:
         # Get rid of black frame
@@ -243,19 +250,35 @@ if __name__ == '__main__':
         mask = cv2.bitwise_or(mask, black_mask)
 
         # Classifier
-        classes = classifyObjs(mask)
+        classes, Centers = classifyObjs(mask)
         classes_gray = cv2.cvtColor(classes, cv2.COLOR_BGR2GRAY)
-        #frm_classes = np.zeros(frame_bgr.shape)
         frm_classes = np.copy(classes)
         frm_classes[classes_gray == 0, :] = frame_bgr[classes_gray == 0, :]
 
-        # Upd the main image
-        image = np.zeros((frame_bgr.shape[0] * 2, frame_bgr.shape[1], frame_bgr.shape[2]), dtype=np.uint8)
-        image[:frame_gray.shape[0], :frame_bgr.shape[1], :] = frame_bgr
-        image[frame_gray.shape[0]:, :frame_bgr.shape[1], :] = frm_classes
+        # Triangles descriptions
+        text_w, text_h = 60, 16
+        for (x, y) in Centers[0]:
+            cv2.rectangle(frm_classes, (int(x - text_w / 2), int(y - text_h / 2)), (int(x + text_w / 2), int(y + text_h / 2)), (255, 255, 255), -1)
+            cv2.putText(frm_classes, 'triangle', (int(x - text_w / 2), int(y + text_h / 2) - 3), cv2.QT_FONT_NORMAL, 0.5, (0, 0, 0), 1)
+
+        # Circle descriptions
+        text_w, text_h = 44, 16
+        for (x, y) in Centers[1]:
+            cv2.rectangle(frm_classes, (int(x - text_w / 2), int(y - text_h / 2)),
+                          (int(x + text_w / 2), int(y + text_h / 2)), (255, 255, 255), -1)
+            cv2.putText(frm_classes, 'circle', (int(x - text_w / 2), int(y + text_h / 2) - 3), cv2.QT_FONT_NORMAL,
+                        0.5, (0, 0, 0), 1)
+
+        # Rectangle descriptions
+        text_w, text_h = 74, 16
+        for (x, y) in Centers[2]:
+            cv2.rectangle(frm_classes, (int(x - text_w / 2), int(y - text_h / 2)),
+                          (int(x + text_w / 2), int(y + text_h / 2)), (255, 255, 255), -1)
+            cv2.putText(frm_classes, 'rectangle', (int(x - text_w / 2), int(y + text_h / 2) - 3), cv2.QT_FONT_NORMAL,
+                        0.5, (0, 0, 0), 1)
 
         # Upd screen
-        cv2.imshow('Objects detection', image)
+        cv2.imshow('Objects classifier', frm_classes)
 
         if key == ord(' '):
             key = cv2.waitKey(0)
@@ -263,15 +286,12 @@ if __name__ == '__main__':
             key = cv2.waitKey(25)
 
         # Write frame in output video
-        frame_out = np.zeros((int(blackframe.shape[0] * 0.8), int(blackframe.shape[1] * 0.8)), dtype='uint8')
-        frame_out[int(60 * 0.8):int(420 * 0.8), :] = mask
+        frame_out = np.zeros((int(blackframe.shape[0] * 0.8), int(blackframe.shape[1] * 0.8), blackframe.shape[2]), dtype='uint8')
+        frame_out[int(60 * 0.8):int(420 * 0.8)] = frm_classes
         writer.write(frame_out)
-
-        #writer2.write(image)
 
         # Get the next frame
         ret, blackframe = vid.read()
     vid.release()
     writer.release()
     cv2.destroyAllWindows()
-    #writer2.release()
