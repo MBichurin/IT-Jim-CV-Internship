4c0cbd9707bba653818b7954b33ad14d739e666b
MBichurin <bichurin.cmds@gmail.com>
2020-07-25 19:52:05 +0300

    OF tracker first tries

--- Task3/ObjectTracking.py
+++ Task3/ObjectTracking.py
@@ -2,16 +2,35 @@ import numpy as np
 import cv2
 
 
+def orb_tracker(img, prev, prev_pts):
+    lk_params = dict(winSize=(35, 35),
+                     maxLevel=4,
+                     criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+    # calculate optical flow
+    pts, st, err = cv2.calcOpticalFlowPyrLK(prev, img, prev_pts, None, **lk_params)
+    successful = (st == 1)
+    if np.sum(successful) == 0:
+        return None
+
+    prev_pts = prev_pts.reshape(-1, 1, 2)
+    pts = pts.reshape(-1, 1, 2)
+    prev_pts = prev_pts[successful]
+    pts = pts[successful]
+    return prev_pts, pts
+
+
+
+
 def orb_detector(img, tmp):
     # ORB
-    orb = cv2.ORB_create(1000)
+    orb = cv2.ORB_create(1000, 1.2, 30)
     # Keypoints and descriptors of img and tmp
     kp_img, des_img = orb.detectAndCompute(img, None)
     kp_tmp, des_tmp = orb.detectAndCompute(tmp, None)
     # Match descriptors of the pics
     matcher = cv2.BFMatcher(cv2.NORM_HAMMING, True)
     matches = matcher.match(des_tmp, des_img)
-    # Leave 10 best matches
+    # Leave the best matches
     matches = sorted(matches, key=lambda x: x.distance)
     selected = []
     for m in matches:
@@ -19,31 +38,47 @@ def orb_detector(img, tmp):
             break
         selected.append(m)
 
-    if len(selected) > 10:
-        # Reshape keypoints to pass them in cv2.findHomography
+    if len(selected) >= 10:
+        # Reshape matched keypoints to pass them in cv2.findHomography
         img_pts = np.float32([kp_img[m.trainIdx].pt for m in selected]).reshape(-1, 1, 2)
         tmp_pts = np.float32([kp_tmp[m.queryIdx].pt for m in selected]).reshape(-1, 1, 2)
+
+        return img_pts, tmp_pts
+    else:
+        return None, None
+
+
+def boundingRect(img, prev, tmp, algo, prev_pts, prev_rect):
+    if (algo == 2) and (prev_pts is not None):
+        prev_pts, img_pts = orb_tracker(img, prev, prev_pts)
+
+        if len(img_pts) >= 10:
+            # Get the homography matrix and the matches mask
+            hom_Mat, matchMask = cv2.findHomography(prev_pts, img_pts, cv2.RANSAC, 5.0)
+            # Deform the rectangle on the detected chocolate
+            rect = cv2.perspectiveTransform(prev_rect, hom_Mat)
+            return img_pts, rect
+
+    img_pts, tmp_pts = orb_detector(img, tmp)
+    if (img_pts is not None) and (tmp_pts is not None):
         # Get the homography matrix to fit template on the frame and the matches mask
         hom_Mat, matchMask = cv2.findHomography(tmp_pts, img_pts, cv2.RANSAC, 5.0)
-        matchMask = matchMask.ravel().tolist()
         # Get the corners of the tmp (not features but top-left, top-right etc.)
         h, w = tmp.shape[:2]
-        frm_corners = np.float32([[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]]).reshape(-1, 1, 2)
+        tmp_corners = np.float32([[0, 0], [w - 1, 0], [w - 1, h - 1], [0, h - 1]]).reshape(-1, 1, 2)
         # Draw a rectangle on the detected chocolate
-        rect = cv2.perspectiveTransform(frm_corners, hom_Mat)
-        img = cv2.polylines(img, [np.int32(rect)], True, (0, 0, 255), 3, cv2.LINE_AA)
-
-        draw_params = dict(matchColor=None,  # draw matches in green color
-                           singlePointColor=None,
-                           matchesMask=matchMask,  # draw only inliers
-                           flags=2)
-        inliers = cv2.drawMatches(tmp, kp_tmp, img, kp_img, selected, None, **draw_params)
-        cv2.imshow('win', cv2.resize(inliers, (int(inliers.shape[1] * 0.5), int(inliers.shape[0] * 0.5))))
+        rect = cv2.perspectiveTransform(tmp_corners, hom_Mat)
     else:
-        cv2.imshow('win', np.zeros_like(img))
+        rect = None
+    return img_pts, rect
+
+
 
 
 if __name__ == '__main__':
+    # Choose the algorithm
+    ALGO = 2
+
     # Load video
     vid_name = 'find_chocolate.mp4'
     vid = cv2.VideoCapture(vid_name)
@@ -59,18 +94,27 @@ if __name__ == '__main__':
 
     key = None
 
+    prev_pts = None
+    prev_rect = None
+    prev = None
+
     while ret:
         frm_gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
         tmp_gray = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('Win', cv2.resize(frm, (int(frm.shape[1] * 0.7), int(frm.shape[0] * 0.7))))
+        prev_pts, prev_rect = boundingRect(frm, prev, tmp, ALGO, prev_pts, prev_rect)
 
-        orb_detector(frm, tmp)
+        prev = np.copy(frm)
+
+        if prev_rect is not None:
+            cv2.polylines(frm, [np.int32(prev_rect)], True, (0, 255, 0), 3)
+
+        cv2.imshow('Win', cv2.resize(frm, (int(frm.shape[1] * 0.7), int(frm.shape[0] * 0.7))))
 
         if key == ord(' '):
             key = cv2.waitKey(0)
         else:
-            key = cv2.waitKey(30)
+            key = cv2.waitKey(1)
 
         # Write frame in output video
         # writer.write(frm)
