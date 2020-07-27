f61e32bc022f6b4c9160f378fbfbfd44e163bd97
MBichurin <bichurin.cmds@gmail.com>
2020-07-27 15:27:55 +0300

    Features don't disappear when they're out of frame borders

--- Task3/ObjectTracking.py
+++ Task3/ObjectTracking.py
@@ -3,40 +3,33 @@ import cv2
 
 
 def of_tracker(img, prev, prev_pts):
-    lk_params = dict(winSize=(15, 15),
+    lk_params = dict(winSize=(30, 30),
                      maxLevel=30,
                      criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
     # calculate optical flow
     pts, st, err = cv2.calcOpticalFlowPyrLK(prev, img, prev_pts, None, **lk_params)
-    print(max(err * st))
+
     # successful = ((st == 1) & (err < 40))
     successful = (st == 1)
     if np.sum(successful) == 0:
         return None, None
 
-    prev_pts = prev_pts.reshape(-1, 1, 2)
-    pts = pts.reshape(-1, 1, 2)
-    prev_pts = prev_pts[successful]
-    pts = pts[successful]
-    return prev_pts, pts
+    return pts, successful
 
 
 def orb_detector(img, tmp):
     # ORB
-    orb = cv2.ORB_create(10000, 1.2, 40)
+    orb = cv2.ORB_create(10000, 1.2, 30)
     # Keypoints and descriptors of img and tmp
     kp_img, des_img = orb.detectAndCompute(img, None)
     kp_tmp, des_tmp = orb.detectAndCompute(tmp, None)
     # Match descriptors of the pics
-    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, True)
-    matches = matcher.match(des_tmp, des_img)
-    # Leave the best matches
-    matches = sorted(matches, key=lambda x: x.distance)
+    matcher = cv2.BFMatcher(cv2.NORM_HAMMING, False)
+    matches = matcher.knnMatch(des_tmp, des_img, k=2)
     selected = []
-    for m in matches:
-        if m.distance > 30:
-            break
-        selected.append(m)
+    for m1, m2 in matches:
+        if m1.distance < 0.8 * m2.distance:
+            selected.append(m1)
 
     if len(selected) >= 10:
         # Reshape matched keypoints to pass them in cv2.findHomography
@@ -52,16 +45,23 @@ def boundingRect(img, prev, tmp, algo, prev_pts, prev_rect):
     key = cv2.waitKey(1)
 
     if (algo == 2) and (prev_pts is not None) and (key != ord('1')):
-        prev_pts, img_pts = of_tracker(img, prev, prev_pts)
+        img_pts, successful = of_tracker(img, prev, prev_pts)
+
+        prev_pts = prev_pts.reshape(-1, 1, 2)
+        img_pts = img_pts.reshape(-1, 1, 2)
 
         if (img_pts is not None) and (len(img_pts) >= 10):
             # Get the homography matrix and the matches mask
-            hom_Mat, matchMask = cv2.findHomography(prev_pts, img_pts, cv2.RANSAC, 10.0)
+            hom_Mat, matchMask = cv2.findHomography(prev_pts[successful], img_pts[successful], cv2.RANSAC, 10.0)
             # Deform the rectangle on the detected chocolate
             rect = cv2.perspectiveTransform(prev_rect, hom_Mat)
+            img_pts = cv2.perspectiveTransform(prev_pts, hom_Mat)
+            img_pts = img_pts.reshape(-1, 2)
 
             if rect is not None:
                 cv2.polylines(frm, [np.int32(rect)], True, (0, 255, 0), 3)
+            for x, y in img_pts:
+                cv2.circle(frm, (x, y), 1, (255, 0, 0), 2)
 
             return img_pts, rect
 
@@ -112,9 +112,11 @@ if __name__ == '__main__':
         frm_gray = cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY)
         tmp_gray = cv2.cvtColor(tmp, cv2.COLOR_BGR2GRAY)
 
+        frm_bUp = np.copy(frm)
+
         prev_pts, prev_rect = boundingRect(frm, prev, tmp, ALGO, prev_pts, prev_rect)
 
-        prev = np.copy(frm)
+        prev = frm_bUp
 
         cv2.imshow('Win', cv2.resize(frm, (int(frm.shape[1] * 0.7), int(frm.shape[0] * 0.7))))
 
